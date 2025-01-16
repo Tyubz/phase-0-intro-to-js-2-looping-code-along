4c91e80c93012509f3f0fc3976b03f5d2d95c833
VictorTubei <vktubei@gmail.com>
2025-01-16 18:15:19 +0300

    Completed looping

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, sherehe) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${sherehe} gift!`);
+    }
+    return messages;
+}
+
+const names = ["Vic", "Otieno", "Nyambura"];
+const event = "birthday";
+const thankYouMessages = writeCards(names, sherehe);
+
+console.log(thankYouMessages);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+countDown(10);
